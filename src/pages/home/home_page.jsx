30d3fff51ae34e521f33f8afeaff3126598c6ce0
Viniciusouza19 <vinicius.sn.me@gmail.com>
2024-04-05 21:26:39 -0300

    bug-fix: firebase aleluia

--- src/pages/home/home_page.jsx
+++ src/pages/home/home_page.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
+import { firestore } from '../../services/firebase'; // Importe o serviço de banco de dados do Firebase
 
 const HomePage = () => {
-    return (
-      
-       
-        <section id="hero" className="d-flex align-items-center justify-content-center">
-          <div className="container" data-aos="fade-up">
-            <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-              <div className="col-xl-6 col-lg-8">
-                <img src="assets/img/logo.png" className="img-fluid" alt=""/>
-                <h1>Barriga Cheia<span>.</span></h1>
-                <h2>Reserve seu lanche, e fuja das filas!</h2>
-              </div>
-            </div>
-            <div className="row gy-4 mt-5 justify-content-center text-decoration-none" data-aos="zoom-in" data-aos-delay="250">
-              <div className="col-xl-2 col-md-4">
-                <div className="icon-box">
-                <i class="ri-bread-line"></i>
-                  <h3><a href="/">Lanches</a></h3>
-                </div>
-              </div>
-              <div className="col-xl-2 col-md-4">
-                <div className="icon-box">
-                  <i className="bi bi-cup-straw"></i>
-                  <h3><a href="/">Bebidas</a></h3>
-                </div>
-              </div>
-              <div className="col-xl-2 col-md-4">
-                <div className="icon-box">
-                  <i className="bi bi-cake"></i>
-                  <h3><a href="/" >Doces</a></h3>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      
-    );
+  const adicionarAoFirestore = () => {
+    const collectionRef = firestore.collection('testes');
+
+    try {
+      collectionRef.add({
+        nome: 'Teste',
+        idade: 20,
+      });
+      console.log('Adicionado ao Firestore com sucesso!');
+    }
+    catch (error) {
+      console.error('Erro ao adicionar ao Firestore:', error);
+    }
+
   };
+
+  return (
+    <section id="hero" className="d-flex align-items-center justify-content-center">
+      <div className="container" data-aos="fade-up">
+        <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+          <div className="col-xl-6 col-lg-8">
+            <img src="assets/img/logo.png" className="img-fluid" alt=""/>
+            <h1>Barriga Cheia<span>.</span></h1>
+            <h2>Reserve seu lanche, e fuja das filas!</h2>
+          </div>
+        </div>
+        <div className="row gy-4 mt-5 justify-content-center text-decoration-none" data-aos="zoom-in" data-aos-delay="250">
+          <div className="col-xl-12 text-center">
+            {/* Botão para adicionar algo ao Firestore */}
+            <button onClick={adicionarAoFirestore} className="btn btn-primary">Adicionar ao Firestore</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default HomePage;
---
+++ src/services/firebase.js
@@ -0,0 +1,18 @@
+// firebase.js
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAA1wu2sQvlzLQvGsXrWheDiEPbpnR0WZY",
+  authDomain: "barriga-lanches.firebaseapp.com",
+  projectId: "barriga-lanches",
+  storageBucket: "barriga-lanches.appspot.com",
+  messagingSenderId: "591751711184",
+  appId: "1:591751711184:web:23a6589c792a426d5a91d5",
+  measurementId: "G-0T520VG5VZ"
+};
+
+// Inicialize o Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const firestore = app.firestore();
